74a6b174f9e05658b03566fdf2667e15ccc65715
naomiko <naomiko@post.bgu.ac.il>
2024-08-19 09:41:21 +0300

    added max limit const

--- ex23.cc
+++ ex23.cc
@@ -3,6 +3,9 @@
 #include <vector>
 #include <array>
 
+
+const int MAX_LIMIT = 28123;
+
 /// Calculates the sum of all divisors of a given number.
 int get_divisors_sum(int num)
 {
@@ -30,8 +33,8 @@ int get_divisors_sum(int num)
 }
 
 /// Initialize the abundants vector and array.
-void initialize_abundants(std::array<bool, 28123>& abundants_array, std::vector<int>& abundants_vector) {
-    for (int i = 12; i < 28123; i++)
+void initialize_abundants(std::array<bool, MAX_LIMIT>& abundants_array, std::vector<int>& abundants_vector) {
+    for (int i = 12; i < MAX_LIMIT; i++)
     {
         if (get_divisors_sum(i) > i)
         {
@@ -47,7 +50,7 @@ void initialize_abundants(std::array<bool, 28123>& abundants_array, std::vector<
 
 
 /// If n is a sum of two abundents - it returns 0, otherwise, it return n.
-int calc_value_to_add(const std::array<bool, 28123>& abundants_array, const std::vector<int>& abundants_vector, int n) {
+int calc_value_to_add(const std::array<bool, MAX_LIMIT>& abundants_array, const std::vector<int>& abundants_vector, int n) {
     for (int current_abundant : abundants_vector)
     {
         if (current_abundant >= n) {
@@ -66,11 +69,11 @@ int calc_value_to_add(const std::array<bool, 28123>& abundants_array, const std:
 int main()
 {
     int sum = 0;
-    std::array<bool, 28123> abundants_array;
+    std::array<bool, MAX_LIMIT> abundants_array;
     std::vector<int> abundants_vector;
     initialize_abundants(abundants_array, abundants_vector);
 
-    for (int i = 1; i < 28123; i++)
+    for (int i = 1; i < MAX_LIMIT; i++)
     {
         sum += calc_value_to_add(abundants_array, abundants_vector, i);
     }
